f84cb339eff262d97c0b59689e9b34764d2e5518
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-13 23:35:54 +0000

    refactoring to use the ES6 object shorthand notation to export an object

--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 
-const connect = require('./client')
+const { connect } = require('./client')
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume(); 
-  
-  //check for the ctrl + c input and terminate the game
-  stdin.on('data', function handleUserInput(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-
-return stdin;
-}
-
 setupInput();
-
