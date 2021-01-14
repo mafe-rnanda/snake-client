59da96bc4dfbaa7ca5f4f71e62eed0fee9c6d848
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-14 01:43:32 +0000

    added movement for our snake and added connection to the input file

---
+++ input.js
@@ -0,0 +1,48 @@
+// Stores the active TCP connection object.
+let connection;
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(conn) {
+  connection = conn;
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume(); 
+  
+  //check for the ctrl + c input and terminate the game
+  stdin.on('data', function handleUserInput(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    else if (key === 'w') {
+      connection.write("Move: up");
+      // console.log("Move: up")
+    }
+    else if (key === 'a') {
+      connection.write("Move: left");
+      // console.log("Move: left")
+    }
+    else if (key === 's') {
+      // console.log("Move: down");
+      connection.write("Move: down");
+      // console.log("Move: down")
+    }
+    else if (key === 'd') {
+      connection.write("Move: right");
+      // console.log("Move: right")
+    }
+    else if (key === 'x') {
+      connection.write("Say: passing through")
+      console.log("Message!")
+    }
+  })
+
+
+return stdin;
+}
+
+module.exports = { setupInput };
